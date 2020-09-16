297f31857ceb962a7fd94a24ea384dfcf46e7fd5
hookem406 <48630328+hookem406@users.noreply.github.com>
2020-09-16 07:30:00 -0500

    update

---
+++ data.py
@@ -0,0 +1,32 @@
+import nltk; nltk.download('stopwords')
+
+import re
+import numpy as np
+import pandas as pd
+from pprint import pprint
+
+# Gensim
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+# spacy for lemmatization
+import spacy
+
+# Plotting tools
+import pyLDAvis
+import pyLDAvis.gensim  # don't skip this
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# Enable logging for gensim - optional
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
+
+import warnings
+warnings.filterwarnings("ignore",category=DeprecationWarning)
+
+df = pd.read_json('https://raw.githubusercontent.com/selva86/datasets/master/newsgroups.json')
+print(df.target_names.unique())
+df.head()
